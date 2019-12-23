b72889bf406a00487532385d270e644cb9a60c9e
awn <hermawans710@gmail.com>
2019-12-23 10:42:41 +0700

    update

--- app/src/main/java/awan/project/wordpressvolley/Config.java
+++ app/src/main/java/awan/project/wordpressvolley/Config.java
@@ -1,5 +1,5 @@
 package awan.project.wordpressvolley;
 
 public class Config {
-    //public static String base_url="http://www.murianews.com/";
+    public static String base_url="Your Url Base";
 }
